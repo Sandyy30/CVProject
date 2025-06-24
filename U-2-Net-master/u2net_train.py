7325c059f9bea83e008de1d97efa0360b36fa4cf
Sandyy30 <98335389+Sandyy30@users.noreply.github.com>
2025-06-25 02:20:48 +0800

    Update u2net_train.py

--- U-2-Net-master/u2net_train.py
+++ U-2-Net-master/u2net_train.py
@@ -50,7 +50,7 @@ def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
 
 model_name = 'u2net' #'u2netp'
 
-# data_dir = os.path.join(os.getcwd(), 'train_data' + os.sep)
+data_dir = os.path.join(os.getcwd(), 'train_data' + os.sep)
 # tra_image_dir = os.path.join('DUTS', 'DUTS-TR', 'DUTS-TR', 'im_aug' + os.sep)
 # tra_label_dir = os.path.join('DUTS', 'DUTS-TR', 'DUTS-TR', 'gt_aug' + os.sep)
 tra_image_dir = '/kaggle/working/CVProject/U-2-Net-master/train_data/ECSSD/'
